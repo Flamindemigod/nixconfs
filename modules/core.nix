b418124318d9636c0cd9bc9a6d0b07b8c9e85558
Flamindemigod <flamin@flamindemigod.com>
2026-01-14 21:54:47 +0000

    added more zsh stuffs

--- modules/core.nix
+++ modules/core.nix
@@ -28,6 +28,12 @@
 
   services.openssh.enable = true;
   nixpkgs.config.allowUnfree = true;
+  environment.sessionVariables = {
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+  };
 
   nix.gc = {
     automatic = true;
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,10 +1,43 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   users.defaultUserShell = pkgs.zsh;
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
+    enableBashCompletion = true;
+    histSize = 10000;
+    histFile = "$XDG_DATA_HOME/zsh/history";
+    shellInit = ''
+      	bindkey ' ' magic-space
+
+      	chpwd() {
+      	  ls
+      	}
+
+      	autoload -Uz add-zsh-hook
+
+      	function auto_nix() {
+      	  # If we're already in a nix develop shell, do nothing
+      	  [[ -n "$IN_NIX_SHELL" ]] && return
+
+      	  # Walk up to find a flake
+      	  local dir="$PWD"
+      	  while [[ "$dir" != "/" ]]; do
+      		if [[ -f "$dir/flake.nix" ]]; then
+      		  nix develop
+      		  return
+      		fi
+      		dir="${dir:h}"
+      	  done
+      	}
+
+      add-zsh-hook chpwd auto_nix
+    '';
     shellAliases = let
       flakeDir = "~/.nix";
     in {
