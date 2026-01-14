ef1984d63d7e183ccdbf6ae55011b8f3de2b9aed
Flamindemigod <flamin@flamindemigod.com>
2026-01-14 16:06:34 +0000

    added git and using recursiveUpdate

--- hosts/satis/home.nix
+++ hosts/satis/home.nix
@@ -10,67 +10,65 @@
   git = import ../../hjem-modules/git.nix {inherit pkgs;};
 in {
   imports = [
-    ../../hjem-modules/core.nix
+	../../hjem-modules/core.nix
   ];
   hjem.users.flamin = {
-    enable = true;
-    directory = "/home/flamin";
-    user = "flamin";
-    rum.desktops.niri =
-      {
-        config = ''
-          	include "dms/colors.kdl"
-          	include "dms/cursor.kdl"
-          	include "dms/layout.kdl"
-          	include "dms/outputs.kdl"
-          	include "dms/wpblur.kdl"
-          	include "dms/alttab.kdl"
-                 prefer-no-csd
-          	input {
-                 	keyboard {
-                 		xkb {
-                 			layout "gb"
-                 			options "caps:swapescape"
-                 		}
-                 		numlock
-                 	}
-                 	touchpad {
-                 		tap
-                 		natural-scroll
-                 		scroll-method "two-finger"
-                 	}
-                 	focus-follows-mouse max-scroll-amount="0%"
-               	warp-mouse-to-focus
-                 }
-                 hotkey-overlay {
-                 	skip-at-startup
-                 }
-                 //For Vel only
-                 screenshot-path "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png"
+	enable = true;
+	directory = "/home/flamin";
+	user = "flamin";
+	rum.desktops.niri = lib.recursiveUpdate niri {
+	  config = ''
+			include "dms/colors.kdl"
+			include "dms/cursor.kdl"
+			include "dms/layout.kdl"
+			include "dms/outputs.kdl"
+			include "dms/wpblur.kdl"
+			include "dms/alttab.kdl"
+			   prefer-no-csd
+			input {
+				keyboard {
+					xkb {
+						layout "gb"
+						options "caps:swapescape"
+					}
+					numlock
+				}
+				touchpad {
+					tap
+					natural-scroll
+					scroll-method "two-finger"
+				}
+				focus-follows-mouse max-scroll-amount="0%"
+				warp-mouse-to-focus
+			   }
+			   hotkey-overlay {
+				skip-at-startup
+			   }
+			   //For Vel only
+			   screenshot-path "~/Pictures/Screenshots/Screenshot from %Y-%m-%d %H-%M-%S.png"
 
-                 window-rule {
-                 match app-id=r#"^org\.keepassxcc\.KeePassXC$"#
-                 block-out-from "screen-capture"
-                 }
+			   window-rule {
+			   match app-id=r#"^org\.keepassxcc\.KeePassXC$"#
+			   block-out-from "screen-capture"
+			   }
 
-                 window-rule {
-                 geometry-corner-radius 12
-                 clip-to-geometry true
-               }
-          layout {
-          	gaps 8
-          }
-        '';
-      }
-      // niri;
-    rum.programs = {
-      git = lib.recursiveUpdate git {
-        settings.user = {
-          name = "Flamindemigod";
-          email = "flamin@flamindemigod.com";
-          #TODO Add Signing Key
-        };
-      };
-    };
+			   window-rule {
+			   geometry-corner-radius 12
+			   clip-to-geometry true
+			 }
+		layout {
+			gaps 8
+		}
+	  '';
+	};
+	rum.programs = {
+	  git = lib.recursiveUpdate git {
+		settings.user = {
+		  name = "Flamindemigod";
+		  email = "flamin@flamindemigod.com";
+		  #TODO Add Signing Key
+		};
+	  };
+	};
   };
 }
