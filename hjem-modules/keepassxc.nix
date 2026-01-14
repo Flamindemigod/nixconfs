705c261e70715b944c618a9e2110f12e84657dbf
Flamindemigod <flamin@flamindemigod.com>
2026-01-14 16:11:23 +0000

    Added keepassxc module

---
+++ hjem-modules/keepassxc.nix
@@ -0,0 +1,22 @@
+{
+  enable = true;
+  settings = {
+    GUI = {
+      ColorPasswords = true;
+      MinimizeOnClose = true;
+      MinimizeOnStartup = true;
+      MinimizeToTray = true;
+      ShowTrayIcon = true;
+      ApplicationTheme = "dark";
+      TrayIconAppearence = "colorful";
+    };
+    Browser = {
+      Enabled = true;
+    };
+    General = {
+      BackupBeforeSave = true;
+      ConfigVersion = 2;
+      MinimizeAfterUnlock = true;
+    };
+  };
+}
