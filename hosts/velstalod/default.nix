fb7027739d2a8e7c2455f2263e76c31353e6b455
Flamindemigod <flamin@flamindemigod.com>
2026-01-22 17:58:31 +0000

    forgot to add hardware var

--- hosts/velstalod/default.nix
+++ hosts/velstalod/default.nix
@@ -2,22 +2,24 @@
   nixpkgs,
   inputs,
   ...
-}:
-nixpkgs.lib.nixosSystem {
-  specialArgs = {inherit inputs;};
-  system = "x86_64-linux";
-  modules = [
-    {
-      networking.hostName = "velstalod";
-      system.stateVersion = "25.11"; #Never Change This!!
-    }
-    hardware.common-cpu-intel-cpu-only
-    hardware.common-gpu-amd
-    hardware.common-pc-laptop
-    inputs.niri.nixosModules.niri
-    inputs.dms.nixosModules.dank-material-shell
-    inputs.dms-plugins.modules.default
-    inputs.hjem.nixosModules.default
-    ./configuration.nix
-  ];
-}
+}: let
+  hardware = inputs.nixos-hardware.nixosModules;
+in
+  nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit inputs;};
+    system = "x86_64-linux";
+    modules = [
+      {
+        networking.hostName = "velstalod";
+        system.stateVersion = "25.11"; #Never Change This!!
+      }
+      hardware.common-cpu-intel-cpu-only
+      hardware.common-gpu-amd
+      hardware.common-pc-laptop
+      inputs.niri.nixosModules.niri
+      inputs.dms.nixosModules.dank-material-shell
+      inputs.dms-plugins.modules.default
+      inputs.hjem.nixosModules.default
+      ./configuration.nix
+    ];
+  }
