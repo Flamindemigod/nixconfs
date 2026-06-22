21dd9001b29a9e7e1fad3c9c5e4a991587f657e8
Flamindemigod <flamin@flamindemigod.com>
2026-06-22 16:00:46 +0100

    made sops module slightly better /s

--- modules/sops.nix
+++ modules/sops.nix
@@ -16,21 +16,15 @@
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
-    secrets = lib.mkMerge [
-      (lib.mkIf (builtins.hasAttr "flamin" config.users.users)
-        {
-          "flamin/ssh" = {
-            owner = "flamin";
-            path = "/home/flamin/.ssh/id_ed25519";
-          };
-        })
-      (lib.mkIf (builtins.hasAttr "velvy" config.users.users)
-        {
-          "velvy/ssh" = {
-            owner = "velvy";
-            path = "/home/velvy/.ssh/id_ed25519";
-          };
-        })
-    ];
+    secrets."flamin/ssh" = lib.mkIf (builtins.hasAttr "flamin" config.users.users)
+      {
+        owner = "flamin";
+        path = "/home/flamin/.ssh/id_ed25519";
+      };
+    secrets."velvy/ssh" = lib.mkIf (builtins.hasAttr "velvy" config.users.users)
+      {
+        owner = "velvy";
+        path = "/home/velvy/.ssh/id_ed25519";
+      };
   };
 }
